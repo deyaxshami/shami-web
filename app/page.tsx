d6d0fb6d49624b611ad36a8dde1ec4fc371f508b
Mohammad Dyaa Addin Shami <deya.shami@icloud.com>
2025-06-28 20:50:44 +0200

    opti

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-7xl text-center">Hallo 4 Fuqing Donkey Shamis</h1>
-        <img src="donkey.png" className="w-4xl" />
+        <Image
+          src={"/donkey.png"}
+          width={1000}
+          height={600}
+          alt="" />
+
       </main>
     </div>
   );
